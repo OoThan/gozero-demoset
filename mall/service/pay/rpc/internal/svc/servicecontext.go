f81f92b74b1f601186e51c3902695f4a361ac22d
maroo <mgoothan.psnl@gmail.com>
2022-09-23 19:06:08 +0630

    #7 #8 #11 #12

---
+++ mall/service/pay/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "go-zero_microservices/mall/service/pay/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
